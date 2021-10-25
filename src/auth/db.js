af0f977b0945db9e249cae7f2fbb41b41b18c854
Jonathan Roby <jroby@change.org>
2021-10-24 23:14:57 -0700

    clean up unused vars

--- src/auth/db.js
+++ src/auth/db.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 
 import {
   getAuth,
-  createUserWithEmailAndPassword,
+  // createUserWithEmailAndPassword,
   fetchSignInMethodsForEmail
 } from "firebase/auth";
 
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const auth = getAuth();
 
