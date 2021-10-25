c900e7517669ac23092e1257485e1e6962b5053e
Jonathan Roby <jroby@change.org>
2021-10-24 17:15:15 -0700

    add email validation

--- src/components/LoginOrSignUp.jsx
+++ src/components/LoginOrSignUp.jsx
@@ -1,5 +1,10 @@
 import { useMachine } from "@xstate/react";
-import { createMachine, assign } from 'xstate';
+import { createMachine, assign } from "xstate";
+
+const validateEmail = email => {
+  const re = /^[^\s@]+@[^\s@]+$/;
+  return re.test(email);
+};
 
 export const machine = createMachine({
   initial: "editing",
@@ -14,32 +19,60 @@ export const machine = createMachine({
             value: (ctx, e) => e.value
           })
         },
+        SUBMIT: "validating"
       },
+      initial: "idle",
+      states: {
+        idle: {},
+        invalid: {}
+      }
     },
+    validating: {
+      always: [
+        {
+          target: "validated",
+          cond: ctx => validateEmail(ctx.value)
+        },
+        {
+          target: "editing.invalid"
+        }
+      ]
+    },
+    validated: {
+      type: "final"
+    }
   }
 });
 
 export default function LoginOrSignUp({ onSubmit }) {
   const [current, send] = useMachine(machine);
-  
+
   const { value } = current.context;
+
+  const editing = current.matches("editing");
+  const invalid = current.matches({ editing: "invalid" });
 
   return (
     <form
       onSubmit={e => {
         e.preventDefault();
+        send("SUBMIT");
       }}
     >
       <h1>Login or sign up</h1>
 
       <span>Email</span>
+
       <input
+        className={invalid ? "input-error" : "none"}
         value={value}
         onChange={e => send({ type: "CHANGE", value: e.target.value })}
       />
-      <button>
-        Continue
-      </button>
+      {invalid ? (
+        <div className="error">Please enter a valid email address.</div>
+      ) : null}
+
+      <button>Continue</button>
     </form>
   );
 }
